f232a41689a1c3a2a100fcd0c10b5120f3b11ade
JewelYueng <414608192@qq.com>
2018-01-31 18:08:16 +0800

    feat(validate)：增加表单验证

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,32 +1,26 @@
 <template>
   <div class='info-page'>
-    <el-form ref='form' status-icon :model='userInfo' label-width="100px" :rules="rules">
+    <el-form ref='form' status-icon label-position="left" :model='userInfo' :rules="rules" label-width="100px">
       <h5>企业信息</h5>
-      <el-form-item label='企业名称'>
+      <el-form-item label='企业名称' prop="company">
         <el-input v-model='userInfo.company'></el-input>
       </el-form-item>
-      <el-form-item label='地址'>
-        <el-row type="flex" align="middle">
-          <el-col :span="8">
+      <el-form-item label='所在省份' prop="province">
             <el-input v-model='userInfo.province'></el-input>
-          </el-col>
-          <el-col :span="2" class="hint-text">省</el-col>
-          <el-col :span="8">
+          </el-form-item>
+      <el-form-item label='所在城市' prop="city">
             <el-input v-model='userInfo.city'></el-input>
-          </el-col>
-          <el-col :span="2" class="hint-text">市</el-col>
-        </el-row>
-      </el-form-item>
-      <el-form-item label='联系人'>
+          </el-form-item>
+      <el-form-item label='联系人' prop="name">
         <el-input v-model='userInfo.name'></el-input>
       </el-form-item>
-      <el-form-item label='职位'>
+      <el-form-item label='职位' prop="position">
         <el-input v-model='userInfo.position'></el-input>
       </el-form-item>
       <el-form-item label='手机号码' prop="mobile">
         <el-input v-model='userInfo.mobile'></el-input>
       </el-form-item>
-      <el-form-item label='座机号码'>
+      <el-form-item label='座机号码' prop="landline">
         <el-input v-model='userInfo.landline'></el-input>
       </el-form-item>
     </el-form>
@@ -49,7 +43,13 @@ export default {
         landline: ''
       },
       rules: {
+        company: [{ required: true, message: '企业名称不能为空' }],
+        city: [{ required: true, message: ' 城市不能为空' }],
+        province: [{ required: true, message: '省份不能为空' }],
+        name: [{ required: true, message: '联系人不能为空' }],
+        position: [{ required: true, message: '职位不能为空' }],
         mobile: [
+          { required: true, message: '手机号码不能为空' },
           {
             validator: (rule, value, callback) => {
               if (value === '') {
@@ -62,7 +62,24 @@ export default {
                 }
               }
             },
-            trigger: 'blur'
+            trigger: 'blur,change'
+          }
+        ],
+        landline: [
+          { required: true, message: '固定电话不能为空' },
+          {
+            validator: (rule, value, callback) => {
+              if (value === '') {
+                callback(new Error('请输入固定电话'))
+              } else {
+                if (!/^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(value)) {
+                  callback(new Error('请输入格式正确的固定电话'))
+                } else {
+                  callback()
+                }
+              }
+            },
+            triiger: 'blur,change'
           }
         ]
       }
@@ -70,20 +87,16 @@ export default {
   },
   methods: {
     goToQuestions () {
-      let flag = 1
-      for (let i in this.userInfo) {
-        if (!this.userInfo[i]) {
-          flag = 0
+      this.$refs['form'].validate(valid => {
+        if (valid) {
+          this.$router.push({
+            name: 'questions',
+            params: { clientInfo: this.userInfo }
+          })
+        } else {
+          this.$message.warning('请完成所有信息填写')
         }
-      }
-      if (flag) {
-        this.$router.push({
-          name: 'questions',
-          params: { clientInfo: this.userInfo }
-        })
-      } else {
-        this.$message.warning('请完成所有信息填写')
-      }
+      })
     }
   }
 }
@@ -96,4 +109,5 @@ h5 {
 .hint-text {
   font-size: 18px;
 }
+
 </style>
