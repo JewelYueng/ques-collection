246f67f08105473c07faa91d487bdc3003b8b9b8
JewelYueng <414608192@qq.com>
2018-02-03 03:30:25 +0800

    fix(style): 修改一下样式

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// The Vue build version to load with the `import` command (runtime-only or
+// standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
 // 引入element
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import App from './App'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+/* eslint-disable*/
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: {
+        App
+    },
+    template: '<App/>'
 })
